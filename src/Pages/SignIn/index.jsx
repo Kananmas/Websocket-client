763f92c1affbdd0f598901df4b2cffa201f5e6cc
Kanan masajedi <Kananmasajedi@gmail.com>
2023-12-30 10:30:36 +0330

    new_commit

--- src/Pages/SignIn/index.jsx
+++ src/Pages/SignIn/index.jsx
@@ -3,7 +3,7 @@ import { useAuth } from "../../hooks/auth.hook";
 import { BtnHolder, Page, PageInput } from "./index.style";
 import { GetUserRequest } from "../../Classes/Requests/UserRequests/GET";
 import { useNavigate } from "react-router-dom";
-import { useLocalStorage } from "../../hooks/localstorage.hook  ";
+import { useLocalStorage } from "../../hooks/localstorage.hook";
 import { useState } from "react";
 import { ErrorWrapper } from "../../components/ErorrWrapper";
 
